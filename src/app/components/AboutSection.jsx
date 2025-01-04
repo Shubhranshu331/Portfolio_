f3701e3f9b2a5124ad45f8c391710281191aa7b2
Shubhranshu <shubhranshu.331@gmail.com>
2025-01-04 22:16:43 +0530

    changes4

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -67,7 +67,7 @@ const AboutSection = () => {
 
     return <section className="text-white">
         <div id="about" className="md:grid md:grid-cols-2 mt-12 gap-8 items-center py-8 px-4 xl:gap16 sm:py-16 xl:px-16 ">
-            <Image src="/images/About_image.png" width={700} height={600} />
+            <Image src="/images/About_image.png" width={700} height={600} alt='About_Image'/>
             <div className= "mt-4 md:mt-0 text-left flex flex-col h-full">
                 <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
                 <p className="text-base lg:text-lg">I am a dedicated Computer Science Engineer with a proven track record in C, Python, and Java. I am currently delving into R to further expand my skillset. I am passionate about innovation and eager to contribute to cutting-edge projects.</p>
