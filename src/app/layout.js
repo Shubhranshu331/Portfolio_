d32f51bf30b13645c03212d2919c37d25dcac3dc
Shubhranshu <143248748+Shubhranshu331@users.noreply.github.com>
2025-06-10 19:46:37 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,9 @@ export const metadata = {
   title: "Shubhranshu's Portfolio",
   description:
     "Welcome to my professional portfolio! This project is built using Next.js, featuring a robust backend developed on Node.js. I have integrated Resend for seamless email communication, and utilized Namecheap for domain registration and private email services. Emails are sent using the POST method to ensure security and efficiency. This portfolio is proudly hosted on Vercel, offering top-notch performance and reliability. Explore my work, and feel free to reach out with any questions or collaboration opportunities! Feel free to customize this description further to suit your unique style! Let me know if there's anything else you need help with.",
+  icons: {
+    icon: '/images/favicon.ico',
+  },
 };
 
 export default function RootLayout({ children }) {
